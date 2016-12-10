9c1907d683ab6539a8c3b2a8110912f5ba0b24cb
HubertLegec <hubert.legec@gmail.com>
2016-12-10 14:07:41 +0100

    Initial Commit

---
+++ i2048Tests/i2048Tests.swift
@@ -0,0 +1,36 @@
+//
+//  i2048Tests.swift
+//  i2048Tests
+//
+//  Created by Hubert Legęć on 10.12.2016.
+//  Copyright © 2016 Hubert Legęć. All rights reserved.
+//
+
+import XCTest
+@testable import i2048
+
+class i2048Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
